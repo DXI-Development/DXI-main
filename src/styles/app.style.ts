c4f2edc16bd052bf497a3323f98ded5b5a3f8f58
winters <winters0727@gmail.com>
2024-09-11 16:43:35 +0900

    chore: 프로젝트 초기 환경 세팅 및 의존성 설치

---
+++ src/styles/app.style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const PageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const PageMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  max-width: 1920px;
+  min-height: 100vh;
+`;
+
+export const PageSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+`;
